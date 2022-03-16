f559985b9cbad711a8942052eb1bd6e469934386
Martin Guillon <martin.guillon@akylas.fr>
2022-03-16 17:20:46 +0100

    chore: eslint

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,29 +1,32 @@
 const ignoreWarnings = new Set(['a11y-no-onchange', 'a11y-label-has-associated-control', 'a11y-mouse-events-have-key-events', 'a11y-mouse-events-have-key-events']);
 module.exports = {
-  root: true,
-  parser: '@typescript-eslint/parser',
-  extends: ['eslint:recommended', 'plugin:@typescript-eslint/recommended', 'prettier'],
-  plugins: ['svelte3', '@typescript-eslint'],
-  overrides: [{ files: ['*.svelte'], processor: 'svelte3/svelte3' }],
-  settings: {
-    'svelte3/ignore-warnings': (w) => ignoreWarnings.has(w && w.code),
-    'svelte3/typescript': true,
-    'svelte3/ignore-styles': (attributes) => {
-      // https://github.com/sveltejs/eslint-plugin-svelte3/issues/10
-      return attributes && attributes.lang && attributes.lang !== 'css'
+    root: true,
+    parser: '@typescript-eslint/parser',
+    extends: ['eslint:recommended', 'plugin:@typescript-eslint/recommended', 'plugin:prettier/recommended'],
+    plugins: ['prettier', 'svelte3', '@typescript-eslint'],
+    overrides: [{ files: ['*.svelte'], processor: 'svelte3/svelte3' }],
+    settings: {
+        'svelte3/ignore-warnings': (w) => ignoreWarnings.has(w && w.code),
+        'svelte3/typescript': true,
+        'svelte3/ignore-styles': (attributes) => {
+            // https://github.com/sveltejs/eslint-plugin-svelte3/issues/10
+            return attributes && attributes.lang && attributes.lang !== 'css';
+        }
     },
-  },
-  parserOptions: {
-    sourceType: 'module',
-    ecmaVersion: 2019,
-  },
-  env: {
-    browser: true,
-    es2017: true,
-    node: true,
-  },
-  rules:{
-    '@typescript-eslint/ban-ts-comment': 'off',
-    '@typescript-eslint/no-explicit-any': 'off',
-  }
-}
+    parserOptions: {
+        sourceType: 'module',
+        project: ['tsconfig.eslint.json'],
+        extraFileExtensions: ['.cjs'],
+        ecmaVersion: 2019
+    },
+    env: {
+        browser: true,
+        es2017: true,
+        node: true
+    },
+    rules: {
+        'prettier/prettier': 'warn',
+        '@typescript-eslint/ban-ts-comment': 'off',
+        '@typescript-eslint/no-explicit-any': 'off'
+    }
+};
