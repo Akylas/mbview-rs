825a738bf0e1650fe16d898421704f0564f9470c
farfromrefug <dev@akylas.fr>
2024-06-24 17:32:18 +0200

    chore: drop node-sass and update carbon css

--- buildSass.mjs
+++ buildSass.mjs
@@ -1,24 +1,17 @@
-import sass from 'node-sass';
+import * as sass from 'sass';
 import fs from 'fs';
 
 const outputPath = './src/carbon.css';
-sass.render(
-  {
-    file: './src/carbon.scss',
-    includePaths: ['./node_modules'],
-    outFile: outputPath,
-  },
-  function (err, result) {
-    if (!err) {
-      // No errors during the compilation, write this result on the disk
-      fs.writeFile(outputPath, result.css, function (err) {
-        if (err) {
-          console.error(err);
-          //file written on disk
-        }
-      });
-    } else {
-      console.error(err);
-    }
-  }
-);
+sass
+  .compileAsync('./src/carbon.scss', {
+    loadPaths: ['./node_modules'],
+  })
+  .then((result) => {
+    fs.writeFile(outputPath, result.css, function (err) {
+      if (err) {
+        console.error(err);
+        //file written on disk
+      }
+    });
+  })
+  .catch((err) => console.error(err));
